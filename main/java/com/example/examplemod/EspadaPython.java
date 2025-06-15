48d0a4f41e79775f97339c61b8db53e551d879dc
João Paulo <joaopaulodev01@gmail.com>
2025-06-15 14:45:50 -0300

    Add files via upload

---
+++ main/java/com/example/examplemod/EspadaPython.java
@@ -0,0 +1,13 @@
+package com.example.examplemod;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
+
+public class EspadaPython extends SwordItem {
+    public EspadaPython(Properties properties) {
+        super(Tiers.DIAMOND, 10000, -2.4F, properties);
+    }
+}
+
